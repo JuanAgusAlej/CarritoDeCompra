83b44b6b7afd399c7e86baea94c43917bc4e17fd
Juan Agustin Alejandro <juan_agustin_alejandro@hotmail.com>
2021-11-27 13:11:47 -0300

    Menu y Agregar producto

--- script/app.js
+++ script/app.js
@@ -1,9 +1,37 @@
 
+
 let menu = function () {
-    let options = 0;
+    let options = 1;
+    let arreglo = []
 
-    while (opcion != 1) {
+    while (opcion != 5) {
         
-    }
+        opcion = parseInt(prompt(`Ingrese una opcion: 1-agregar 2-eliminar producto 3-filtrat elemento 4-lista ordenada 5-Salir`))
+        
+        switch (opcion) {
+            case 1:
+                    arreglo.push(agregarProducto())
+                break
+            case 2:
 
+                break
+            case 3:
+
+                break
+            case 4:
+
+                break
+            case 5:
+                return arreglo;
+            
+            default:
+                opcion = parseInt(prompt(`Ingrese una opcion: 1-agregar 2-eliminar producto 3-filtrat elemento 4-lista ordenada 5-Salir`))
+        }
+    }
 }
+
+let agregarProducto = function () {
+    return prompt("Ingresar producto: ")
+}
+
+
